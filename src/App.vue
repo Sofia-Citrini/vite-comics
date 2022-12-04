7e0ab9d3d1e0bdaeec4cab0f5efca6b863cf4fcf
Sofia-Citrini <sofiacitrini@gmail.com>
2022-12-04 11:42:36 +0100

    completed BannerFooter component

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,34 @@
 
 <template>
   <TheHeader></TheHeader>
+
+  <main>
+    <div class="content">Content</div>
+  </main>
+
   <TheFooter></TheFooter>
-  
+  <BannerFooter></BannerFooter>
 </template>
 
 <script>
   import TheHeader from './components/TheHeader.vue';
   import TheFooter from './components/TheFooter.vue';
+import BannerFooter from './components/BannerFooter.vue';
 
   export default{
     data() {
     },
     methods: {},
-    components: { TheHeader, TheFooter }
+    components: { TheHeader, TheFooter, BannerFooter }
   }
 </script>
 
 <style lang="scss">
-  @use "./styles/general.scss"
+  @use "./styles/general.scss";
+
+  .content{
+    height: 100px;
+    color: white;
+    background-color: black;
+  }
 </style>
---
+++ src/components/BannerFooter.vue
@@ -0,0 +1,62 @@
+<template>
+    <section class="banner py-4">
+        <div class="container d-flex justify-content-between">
+            <div>
+                <button>sign up now!</button>
+            </div>
+
+            <div class="social">
+                <div class="text">follow us</div>
+                <img src="/imgs/footer-facebook.png" class="icon-social" alt="">
+                <img src="/imgs/footer-twitter.png" class="icon-social" alt="">
+                <img src="/imgs/footer-youtube.png" class="icon-social" alt="">
+                <img src="/imgs/footer-pinterest.png" class="icon-social" alt="">
+                <img src="/imgs/footer-periscope.png" class="icon-social" alt="">
+            </div>
+        </div>
+    </section>
+    
+</template>
+
+<script>
+    export default{
+        data (){
+        },
+        methods: {
+        }
+            
+    }
+</script>
+
+<style scoped lang="scss">
+    @use '../styles/partials/variables' as *;
+
+    .banner{
+        background-color: $color-secondary;
+    }
+
+    button{
+        border: 2px solid $color-primary;
+        background-color: transparent;
+        text-transform: uppercase;
+        color: white;
+        padding: .5rem;
+    }
+
+    .social{
+        display: flex;
+        align-items: center;
+
+        .text{
+            color: $color-primary;
+            text-transform: uppercase;
+            padding: 0 .5rem;
+        }
+
+        img{
+            height: 1.5rem;
+            margin: 0 .5rem;
+        }
+    }
+
+</style>
